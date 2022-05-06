96aafe8a4c969be7538965d578dea616ea68cbd9
phil-gab99 <pgabriel999@hotmail.com>
2022-05-06 05:36:32 -0400

    Updated config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,89 +1,184 @@
-c.tabs.tabs_are_windows = False
-c.tabs.show = "multiple"
-c.tabs.last_close = "close"
+################################################################################
+# ALIASES
+################################################################################
 
+# Type - dict
+c.aliases = {
+    'w': 'session-save',
+    'q': 'close',
+    'qa': 'quit',
+    'wq': 'quit --save',
+    'wqa': 'quit --save'
+}
+
+################################################################################
+# TABS AND WINDOW MANAGEMENT
+################################################################################
+
+# Open new window for every tab
+# Type - Bool
+c.tabs.tabs_are_windows = False
+
+# When to show tab bar
+# Type - String
+# Values - always    -> Always show tab bar
+#          never     -> Always hide tab bar
+#          multuple  -> Hide tab bar if only one tab open
+#          switching -> Show tab bar when switching tabs
+c.tabs.show = 'multiple'
+
+# Behaviour on last closed tab
+# Type - String
+# Values - ignore       -> Do nothing
+#          blank        -> Load blank page
+#          startpage    -> Load start page
+#          default-page -> Load default page
+#          close        -> Close window
+c.tabs.last_close = 'close'
+
+# Auto save session
+# Type - Bool
 c.auto_save.session = True
+
+# Enable smooth scrolling
+# Type - Bool
 c.scrolling.smooth = True
+
+# Load a restored tab as soon as it takes focus.
+# Type - Bool
 c.session.lazy_restore = True
 
-# Scale pages and UI better for hidpi
-# c.zoom.default = "100"
-c.fonts.hints = "bold 12pt monospace"
+################################################################################
+# FONTS AND THEMES
+################################################################################
 
-# Better default fonts
-c.fonts.web.family.standard = "Bitstream Vera Sans"
-c.fonts.web.family.serif = "Bitstream Vera Serif"
-c.fonts.web.family.sans_serif = "Bitstream Vera Sans"
-c.fonts.web.family.fixed = "Fira Mono"
-c.fonts.statusbar = "12pt Cantarell"
+# Font family for standard fonts
+# Type - FontFamily
+c.fonts.web.family.standard = 'Bitstream Vera Sans'
 
-# Use dark mode where possible
+# Font family for serif fonts
+# Type - FontFamily
+c.fonts.web.family.serif = 'Bitstream Vera Serif'
+
+# Font family for sans-serif fonts.
+# Type: FontFamily
+c.fonts.web.family.sans_serif = 'Bitstream Vera Sans'
+
+# Font family for fixed fonts.
+# Type: FontFamily
+c.fonts.web.family.fixed = 'Fira Mono'
+
+# Font used for hints
+# Type - Font
+c.fonts.hints = 'bold 12pt monospace'
+
+# Font used in statusbar
+# Type - Font
+c.fonts.statusbar = '12pt Cantarell'
+
+# Render web contents using dark theme
+# Type - Bool
 c.colors.webpage.darkmode.enabled = True
-c.colors.webpage.darkmode.policy.images = "never"
-c.colors.webpage.bg = "black"
+
+# Which images to apply dark mode to
+# Type - String
+# Values - always -> Apply dark mode filter to all images
+#          never  -> Never apply dark mode filter to any images
+#          smart  -> Apply dark mode based on image content
+c.colors.webpage.darkmode.policy.images = 'never'
+
+# Background color for webpages
+# Type - QtColor
+c.colors.webpage.bg = 'black'
+
+################################################################################
+# EDITING
+################################################################################
 
 # Automatically turn on insert mode when a loaded page focuses a text field
+# Type - Bool
 c.input.insert_mode.auto_load = True
 
 # Edit fields in Emacs with Ctrl+E
+# Type - ShellCommand
 c.editor.command = ["emacsclient", "+{line}:{column}", "{file}"]
 
-# Content
-c.content.pdfjs = True
+################################################################################
+# CONTENT VIEWING
+################################################################################
+
+# Allow pdf.js to view PDF files in browser
+# Type - Bool
+c.content.pdfjs = False
+
+# Autoplay video elements
+# Type - Bool
 c.content.autoplay = False
 
-# Adblocking
+################################################################################
+# ADBLOCKING - TODO
+################################################################################
+
 # c.content.blocking.enabled = True
-# c.content.blocking.method = "adblock"
+# c.content.blocking.method = 'adblock'
 # c.content.blocking.adblock.lists = [
-#     "https://easylist.to/easylist/easylist.txt",
-#     "https://easylist.to/easylist/easyprivacy.txt",
-#     "https://easylist.to/easylist/fanboy-annoyance.txt",
-#     "https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt",
-#     "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
-#     "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
-#     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt",
-#     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt",
-#     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt",
-#     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt",
-#     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt",
-#     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt",
-#     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt"
-#     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt",
-#     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt",
-#     "https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt",
-#     "https://www.malwaredomainlist.com/hostslist/hosts.txt",
-#     "https://www.i-dont-care-about-cookies.eu/abp/",
-#     "https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts",
-#     "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext"
+#     'https://easylist.to/easylist/easylist.txt',
+#     'https://easylist.to/easylist/easyprivacy.txt',
+#     'https://easylist.to/easylist/fanboy-annoyance.txt',
+#     'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt',
+#     'https://secure.fanboy.co.nz/fanboy-annoyance.txt',
+#     'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt',
+#     'https://github.com/uBlockOrigin/uAssets/raw/master/filters/legacy.txt',
+#     'https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters.txt',
+#     'https://github.com/uBlockOrigin/uAssets/raw/master/filters/annoyances.txt',
+#     'https://github.com/uBlockOrigin/uAssets/raw/master/filters/badware.txt',
+#     'https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt',
+#     'https://github.com/uBlockOrigin/uAssets/raw/master/filters/resource-abuse.txt',
+#     'https://github.com/uBlockOrigin/uAssets/raw/master/filters/unbreak.txt'
+#     'https://github.com/uBlockOrigin/uAssets/raw/master/filters/badlists.txt',
+#     'https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2020.txt',
+#     'https://github.com/uBlockOrigin/uAssets/raw/master/filters/filters-2021.txt',
+#     'https://www.malwaredomainlist.com/hostslist/hosts.txt',
+#     'https://www.i-dont-care-about-cookies.eu/abp/',
+#     'https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts',
+#     'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext'
 # ]
 
-# Rendering issues
-c.qt.args = ["disable-seccomp-filter-sandbox"]
+################################################################################
+# KEYBINDINGS
+################################################################################
 
-# Make Ctrl+g quit everything like in Emacs
-config.bind('<Ctrl-g>', 'mode-leave', mode='insert')
-config.bind('<Ctrl-g>', 'mode-leave', mode='command')
-config.bind('<Ctrl-g>', 'mode-leave', mode='prompt')
-config.bind('<Ctrl-g>', 'mode-leave', mode='hint')
-
-# Mpv spawn
-config.bind('wm', 'spawn umpv {url}')
-config.bind('wM', 'hint links spawn umpv {hint-url}')
-
-# Tweak some keybindings
-config.unbind('d') # Don't close tab on lower-case 'd'
-config.bind('yy', 'yank')
+## Normal mode ##
 
 # Swap J and K for tab switching
 config.bind('J', 'tab-prev')
 config.bind('K', 'tab-next')
+config.unbind('d') # Don't close tab on lower-case 'd'
+config.bind('yy', 'yank')
+config.bind('Wm', 'spawn mpv {url}')
+config.bind('WM', 'hint links spawn mpv {hint-url}')
 
-# Vim-style movement keys in command mode
+## Command mode ##
+config.bind('<Ctrl-g>', 'mode-leave', mode='command')
 config.bind('<Ctrl-j>', 'completion-item-focus --history next', mode='command')
 config.bind('<Ctrl-k>', 'completion-item-focus --history prev', mode='command')
 
-# More binding hints here: https://gitlab.com/Kaligule/qutebrowser-emacs-config/blob/master/config.py
+## Hint mode ##
+config.bind('<Ctrl-g>', 'mode-leave', mode='hint')
+
+## Insert mode ##
+config.bind('<Ctrl-g>', 'mode-leave', mode='insert')
+
+## Prompt mode ##
+config.bind('<Ctrl-g>', 'mode-leave', mode='prompt')
+
+################################################################################
+# MISC
+################################################################################
+
+# Additional arguments to pass to Qt, without leading '--'
+# Type - List of String
+c.qt.args = ['disable-seccomp-filter-sandbox'] # Fix rendering issue
 
 # Load the autoconfig file (quteconfig.py)
 config.load_autoconfig()
