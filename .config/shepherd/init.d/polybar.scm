c93c50041dd2e79490cce54ccc65ec96b9090d3e
phil-gab99 <pgabriel999@hotmail.com>
2022-08-30 01:37:43 -0400

    Minor update to config

--- .config/shepherd/init.d/polybar.scm
+++
@@ -1,9 +0,0 @@
-(define polybar
-  (make <service>
-    #:provides '(polybar)
-    #:docstring "Runs `polybar'"
-    #:respawn? #t
-    #:start (make-system-constructor "polybar panel")
-    #:stop (make-kill-destructor)))
-
-(register-services polybar)
