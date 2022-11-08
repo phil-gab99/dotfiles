1a8a7da3d31bbcd3c2b1e6a898295a10795ea5a8
Philippe Gabriel <pgabriel999@hotmail.com>
2022-11-08 09:53:08 -0500

    Channel update

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -3,9 +3,37 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "9e4632081ff31bf0d1715edd66f514614c6dc4bb")
+          "1b117fc3a5623d582758e890b4ab64d0637368cd")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
             (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "cbc4c651add86e36f93bc52fac3747be92f8d842"))
+      (channel
+        (name 'rde)
+        (url "https://git.sr.ht/~abcdw/rde")
+        (branch "master")
+        (commit
+          "6ccfe3911f5f3d12c7e8b0d836832f3ef1bb6011")
+        (introduction
+          (make-channel-introduction
+            "257cebd587b66e4d865b3537a9a88cccd7107c95"
+            (openpgp-fingerprint
+              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+      (channel
+        (name 'flat)
+        (url "https://github.com/flatwhatson/guix-channel.git")
+        (branch "master")
+        (commit
+          "e57424b680e1724105e2598b68c30084b180cf58")
+        (introduction
+          (make-channel-introduction
+            "33f86a4b48205c0dc19d7c036c85393f0766f806"
+            (openpgp-fingerprint
+              "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490")))))
